b37f4e3e350fdd921f92b97c7b6895ff21317c8c
PrasiddhaKhadka <prasiddhak77@gmail.com>
2025-11-21 06:57:35 +0545

    Add: Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import LoginView from "./views/Auth/Login-View"
+
+function App() {
+
+  return (
+    <>
+    <LoginView/>
+     
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/views/Auth/Login-View.jsx
@@ -0,0 +1,9 @@
+function LoginView (){
+    return <div>
+           <h1 className="text-3xl font-bold underline ">
+      Hello world!
+    </h1>
+    </div>
+}
+
+export default LoginView;
